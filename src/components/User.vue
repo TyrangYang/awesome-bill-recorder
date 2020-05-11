1db7ced006b6041af086b43d4a0dc60a59a8f0ee
Haolin Yang <haolinyang95@gmail.com>
2020-05-11 19:15:53 -0400

    init vue.js

---
+++ src/components/User.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="user-card">
+    <h2>User List</h2>
+    <ui>
+      <li>name1</li>
+      <li>name2</li>
+      <li>name3</li>
+    </ui>
+
+    <div class="btn">Add new user</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style scoped>
+.user-card {
+  width: 20%;
+  border: 1px solid #333;
+  border-radius: 5px;
+  text-align: center;
+}
+</style>
